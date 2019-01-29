836eb37d2de0292bdb0e1a04638e32b448cff324
Alex <du.alexcruz@gmail.com>
2019-01-29 16:25:34 -0600

    Doze: Use our custom doze package
    Change-Id: I1822f961544a6225a15f5f12ca4f2453c4bd4566

--- common.mk
+++ common.mk
@@ -20,7 +20,6 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # Keyhandler
 PRODUCT_PACKAGES += \
     ConfigPanel \
-    OneplusDoze \
     com.cyanogenmod.keyhandler
 
 PRODUCT_SYSTEM_SERVER_JARS += com.cyanogenmod.keyhandler
